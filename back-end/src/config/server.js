fe98a882dca8fda9e725ba2c70ec11a76bebc416
Otaviano Montes Zibetti <otavianozibetti@rede.social>
2019-11-11 11:51:44 -0300

    Add first version of back-end

--- back-end/src/config/server.js
+++ back-end/src/config/server.js
@@ -1,9 +1,14 @@
-const port = 3003
+const port = 5050
 const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
+const CORS = require('./cors')
 
-server.use(bodyParser.urlencoded({ extended:true }))
+server.use(bodyParser.urlencoded({ extended:true })) 
 server.use(bodyParser.json())
-
+server.use(bodyParser.json({type:'application/vnd.api+json'}));
+server.use(CORS)
+ 
 server.listen(port, () => console.log(`back-end is running on port ${port}`))
+
+module.exports = server
